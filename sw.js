4e85c15a24f2e93fc20ae668ac30ee05838a4f60
tulikaff659 <tolqinjonofficel@gmail.com>
2026-05-23 10:47:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// sw.js
-const CACHE_NAME = '1xbet-signals-v1';
+const CACHE_NAME = 'megapari-v1';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -8,22 +7,12 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
